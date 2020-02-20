39fad1ce271bdda5f713126cff12cdf16bb6de7f
jackfrued <jackfrued@gmail.com>
2020-02-20 16:58:27 +0800

    第04天下午内容

--- api/helpers.py
+++ api/helpers.py
@@ -2,11 +2,13 @@ from rest_framework.pagination import PageNumberPagination, CursorPagination
 
 
 class CustomPagePagination(PageNumberPagination):
+    """自定义页码分页类"""
     page_size_query_param = 'size'
     max_page_size = 50
 
 
 class AgentCursorPagination(CursorPagination):
+    """经理人游标分页类"""
     page_size_query_param = 'size'
     max_page_size = 50
     ordering = '-agentid'
--- api/views.py
+++ api/views.py
@@ -1,4 +1,9 @@
+import pickle
+
 from django.db.models import Prefetch
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django_redis import get_redis_connection
 from rest_framework.decorators import api_view
 from rest_framework.generics import RetrieveUpdateAPIView, ListCreateAPIView
 from rest_framework.response import Response
@@ -9,6 +14,7 @@ from api.serializers import *
 from common.models import District, Agent, HouseType
 
 
+@cache_page(timeout=365 * 86400)
 @api_view(('GET', ))
 def get_provinces(request):
     """获取省级行政单位"""
@@ -22,11 +28,28 @@ def get_provinces(request):
     })
 
 
+# @api_view(('GET', ))
+# def get_district(request, distid):
+#     """获取地区详情"""
+#     district = caches['default'].get(f'district:{distid}')
+#     if district is None:
+#         district = District.objects.filter(distid=distid).first()
+#         caches['default'].set(f'district:{distid}', district)
+#     serializer = DistrictDetailSerializer(district)
+#     return Response(serializer.data)
+
+
 @api_view(('GET', ))
 def get_district(request, distid):
     """获取地区详情"""
-    district = District.objects.filter(distid=distid)\
-        .defer('parent').first()
+    redis_cli = get_redis_connection()
+    data = redis_cli.get(f'zufang:district:{distid}')
+    if data:
+        district = pickle.loads(data)
+    else:
+        district = District.objects.filter(distid=distid).first()
+        data = pickle.dumps(district)
+        redis_cli.set(f'zufang:district:{distid}', data)
     serializer = DistrictDetailSerializer(district)
     return Response(serializer.data)
 
@@ -57,6 +80,8 @@ class AgentView(RetrieveUpdateAPIView, ListCreateAPIView):
         return cls.get(self, request, *args, **kwargs)
 
 
+@method_decorator(decorator=cache_page(timeout=86400), name='list')
+@method_decorator(decorator=cache_page(timeout=86400), name='retrieve')
 class HouseTypeViewSet(ModelViewSet):
     """户型视图集"""
     queryset = HouseType.objects.all()
@@ -64,7 +89,9 @@ class HouseTypeViewSet(ModelViewSet):
     pagination_class = None
 
 
-class EstateViewSet(ReadOnlyModelViewSet):
+@method_decorator(decorator=cache_page(timeout=86400), name='list')
+@method_decorator(decorator=cache_page(timeout=86400), name='retrieve')
+class EstateViewSet(ModelViewSet):
     """楼盘视图集"""
     queryset = Estate.objects.all()
 
